978e51a79a87dba934e381e3a2e5a4b9d8f9e7b6
soratanmer <soratanmer@gmail.com>
2023-05-19 22:47:25 +0800

    add auto import

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,40 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/ // .vue
+      ],
+
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        'vue-router'
+      ]
+    }),
+    Components({
+      // relative paths to the directory to search for components.
+      dirs: ['src/'],
+
+      // valid file extensions for components.
+      extensions: ['vue'],
+
+      // filters for transforming targets
+      include: [/\.vue$/, /\.vue\?vue/]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
