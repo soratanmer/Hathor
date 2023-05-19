af0ad4925cefa4be7f89058a129493b5ced88faf
soratanmer <soratanmer@gmail.com>
2023-05-19 23:30:23 +0800

    add env and change server config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, UserConfig, ConfigEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -8,40 +8,57 @@ import Components from 'unplugin-vue-components/vite'
 import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    AutoImport({
-      // targets to transform
-      include: [
-        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-        /\.vue$/,
-        /\.vue\?vue/ // .vue
-      ],
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  const env = loadEnv(mode, process.cwd())
 
-      // global imports to register
-      imports: [
-        // presets
-        'vue',
-        'vue-router'
-      ]
-    }),
-    Components({
-      // relative paths to the directory to search for components.
-      dirs: ['src/'],
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        // targets to transform
+        include: [
+          /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+          /\.vue$/,
+          /\.vue\?vue/ // .vue
+        ],
 
-      // valid file extensions for components.
-      extensions: ['vue'],
+        // global imports to register
+        imports: [
+          // presets
+          'vue',
+          'vue-router'
+        ]
+      }),
+      Components({
+        // relative paths to the directory to search for components.
+        dirs: ['src/'],
 
-      // filters for transforming targets
-      include: [/\.vue$/, /\.vue\?vue/]
-    }),
-    UnoCSS(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+        // valid file extensions for components.
+        extensions: ['vue'],
+
+        // filters for transforming targets
+        include: [/\.vue$/, /\.vue\?vue/]
+      }),
+      UnoCSS()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      port: Number(env.VITE_APP_PORT),
+      open: true, // 运行是否自动打开浏览器
+      proxy: {
+        // 反向代理解决跨域
+        [env.VITE_APP_API]: {
+          target: 'http://localhost:3000',
+          // target: 'https://netease-cloud-music-api-soratanmer.vercel.app/',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(new RegExp('^' + env.VITE_APP_API), '') // 替换 /dev-api 为 target 接口地址
+        }
+      }
     }
   }
 })
